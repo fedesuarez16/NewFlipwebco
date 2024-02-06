b47cae0de5234ce8e2e6a32be154e0badd19d8c4
fedesuarez16 <fedesuarez16@gmail.com>
2024-02-06 20:20:03 -0300

    grph

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,13 @@
+// RootLayout.js
+
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { NextSeo } from 'next-seo'; // Import NextSeo
 import Header from '../components/Header';
-import { NextSeo } from 'next-seo';
+import Head from 'next/head'; // Import Head from next/head
 
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -14,29 +17,31 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-         <NextSeo
-        title="Desarrollo Software - Flipwebco"
-        description="Somos la reinvención"
-        openGraph={{
-          title: 'Desarrollo de Software ',
-          description: 'Diseño y desarrollo de productos digitales para empresas',
-          images: [
-            {
-              url: 'https://ibb.co/QM27RMB',
-              width: 1200,
-              height: 630,
-              alt: 'Descripción de la imagen',
-            },
-          ],
-          type: 'website',
-          url: 'https://www.flipwebco.com/',
-          site_name: 'Flipwebco',
-        }}
-      />
+      <Head>
+        {/* Move NextSeo component here */}
+        <NextSeo
+          title="Desarrollo Software - Flipwebco"
+          description="Somos la reinvención"
+          openGraph={{
+            title: 'Desarrollo de Software ',
+            description: 'Diseño y desarrollo de productos digitales para empresas',
+            images: [
+              {
+                url: 'URL de la imagen Open Graph',
+                width: 1200,
+                height: 630,
+                alt: 'Descripción de la imagen',
+              },
+            ],
+            type: 'website',
+            url: 'https://www.flipwebco.com/',
+            site_name: 'Flipwebco',
+          }}
+        />
+      </Head>
       
       <body className={inter.className}>
         <Header />
-
         {children}
       </body>
     </html>
