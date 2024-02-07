e7b1aa88f6f19f86530fc5257486dceb6bdc283a
fedesuarez16 <fedesuarez16@gmail.com>
2024-02-07 13:40:17 -0300

    idkkk

--- pages/sistemas/index.jsx
+++ pages/sistemas/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styles from './style.module.scss';
-import Headersist from '@/components/Headersist';
+import Header from '@/components/HeaderSist';
 import IntroSist from '@/components/IntroSist';
 import SistColumn from '@/components/SistColumn';
 import Contact from '@/components/Contact'
@@ -12,7 +12,7 @@ import Contact from '@/components/Contact'
 export default function Sistemas () {
   return (
     <div className={styles.mainn}>
-        <Headersist/>
+        <Header/>
         <IntroSist/>
         <SistColumn/>
         <Contact/>
