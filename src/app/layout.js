c93650cebbbfb36e25da83aa318abc8505b8961f
fedesuarez16 <fedesuarez16@gmail.com>
2024-02-08 12:32:23 -0300

    grap image

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,13 +11,14 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Flipwebco - Software | Haciendo la reinvención empresarial tecnológica',
-  description: 'Impulsando la transición de las empresas hacia la reinvención digital. Estamos enfocados en la facilitación de procesos empresariales a través de productos digitales',
+  description: 'Impulsando la transición de las empresas hacia la reinvención digital. Estamos enfocados en la facilitación de procesos empresariales a través de productos digitales exepcionales ',
   type: 'website', // Specify the type of content (website, article, etc.)
   locale: 'es_ES', // Specify the locale (language and country)
   url: 'https://www.flipwebco.com/', // Specify the canonical URL of the page
   site_name: 'Flipwebco', // Specify the name of the website
   images: [
     {
+      url: 'https://ibb.co/cQ4hnKD', 
       width: 1200, // Width of the image
       height: 630, // Height of the image
       alt: 'Flipwebco', // Alt text for the image
