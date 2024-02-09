359b4b5ec790daf558cde496fcef344553a1d3dd
fedesuarez16 <fedesuarez16@gmail.com>
2024-02-09 13:43:25 -0300

    fix

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -7,66 +7,53 @@ import { useInView } from 'react-intersection-observer';
 import Rounded from '../../common/RoundedButton';
 import { motion } from 'framer-motion'; 
 
-
-
-
 const inter = Inter({ subsets: ['latin'] });
 
+export default function Hero() {
+  const handleWhatsappClick = () => {
+    window.open('https://api.whatsapp.com/send?phone=5491133370937', '_blank');
+  };
 
-export default function Hero ()  {
+  const [inViewRef, inView] = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
 
-    const handleWhatsappClick = () => {
-        window.open('https://api.whatsapp.com/send?phone=5491133370937', '_blank');
-      };
-
-    const [inViewRef, inView] = useInView({
-        triggerOnce: true,
-        threshold: 0.1,
-      });
-
-      const [buttonInViewRef, buttonInView] = useInView({
-        triggerOnce: true,
-        threshold: 0.1,
-      })
-
-      console.log(buttonInView); // Agrega esta línea
-
+  const [buttonInViewRef, buttonInView] = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
 
   return (
-
- <section className={inter.className}>
-    <div className={styles.hero}>
-      <div className={styles.text}>  
-        <p  ref={inViewRef} >
+    <section className={inter.className}>
+      <div className={styles.hero} ref={inViewRef}>
+        <div className={styles.text}>
+          <p>
             {inView && (
-        <TypeAnimation 
-          sequence={[
-            'Listo para acelerar tu viaje hacia la reinvencion digital?', 
-            1000,
-          ]}
-          speed={60}
-          wrapper='p'
-          repeat={Infinity}
-        /> 
-        
-      )} 
-      </p>
-      </div>
+              <TypeAnimation
+                sequence={[
+                  'Listo para acelerar tu viaje hacia la reinvencion digital?',
+                  1000,
+                ]}
+                speed={60}
+                wrapper='p'
+                repeat={Infinity}
+              />
+            )}
+          </p>
+        </div>
 
-      <motion.div
+        <motion.div
           className={styles.roundedButton}
           ref={buttonInViewRef}
           initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
+          animate={buttonInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 }}
           transition={{ delay: 1.5 }}
         >
-          {buttonInView && ( // Verifica si el botón está en la vista
-            <Rounded onClick={handleWhatsappClick}>Listo para comenzar?</Rounded>
-          )}
+          <Rounded onClick={handleWhatsappClick}>Listo para comenzar?</Rounded>
         </motion.div>
 
-    </div>
+      </div>
     </section>
   );
-};
-
+}
