2e5b1799b62c5eedf38540d8613fe9596691382e
fedesuarez16 <fedesuarez16@gmail.com>
2024-02-15 19:38:17 -0300

    idk

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
