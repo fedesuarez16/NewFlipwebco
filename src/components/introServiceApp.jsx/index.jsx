aac96e45ec0dbdaff59f4cfae78c10d5505b90b5
fedesuarez16 <fedesuarez16@gmail.com>
2024-04-02 19:51:35 -0300

    idk

--- src/components/introServiceApp.jsx/index.jsx
+++ src/components/introServiceApp.jsx/index.jsx
@@ -14,13 +14,13 @@ export default function Index() {
 
   return (
     <section className={inter.className}>
-      <div className={styles.introText}>
-        <p>"Un producto digital que impulse el crecimiento de tu negocio más allá de los límites."</p>
-      </div>
 
       <div className={styles.imageContainer}>
         {/* Use Next.js Image component to import the image */}
         <Image src="/images/vector.png" alt="Description of the image" width={500} height={400} />
+      </div>
+      <div className={styles.introText}>
+        <p>"Un producto digital que impulse el crecimiento de tu negocio más allá de los límites."</p>
       </div>
 
      
