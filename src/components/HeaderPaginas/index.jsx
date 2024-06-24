4f35e86dd7f33c353a7cc05891637f78847d6a3f
fedesuarez16 <fedesuarez16@gmail.com>
2024-06-24 02:04:32 -0300

    idk

--- src/components/HeaderPaginas/index.jsx
+++ src/components/HeaderPaginas/index.jsx
@@ -6,28 +6,33 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 const Hero = () => {
-  return (
-    <section >
-        <div id='inicio' className={`${styles.hero} ${inter.className}`}>
+  const handleScroll = () => {
+    const beneficiosSection = document.getElementById('beneficios');
+    if (beneficiosSection) {
+      beneficiosSection.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
-      <div className={styles.heroContent}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>La Página Web <br /> para tu negocio a solo ARS15.900/mes</h1>
-          <p className={styles.description}>
-            ¡Transforma tu negocio con una página web profesional! Atrae más clientes, mejora tu presencia en línea y destaca frente a la competencia. Diseño personalizado, desarrollo responsivo y optimización SEO incluidos.
-          </p>
-          <div className={styles.buttonContainer}>
-            <Rounded Color={"#fcfcfc"}>
-              <p className={styles.button}>Comenzar</p>
-            </Rounded>
+  return (
+    <section>
+      <div id='inicio' className={`${styles.hero} ${inter.className}`}>
+        <div className={styles.heroContent}>
+          <div className={styles.textContainer}>
+            <h1 className={styles.title}>La Página Web <br /> para tu negocio a solo ARS15.900/mes</h1>
+            <p className={styles.description}>
+              ¡Transforma tu negocio con una página web profesional! Atrae más clientes, mejora tu presencia en línea y destaca frente a la competencia. Diseño personalizado, desarrollo responsivo y optimización SEO incluidos.
+            </p>
+            <div className={styles.buttonContainer} onClick={handleScroll}>
+              <Rounded Color={"#fcfcfc"}>
+                <p className={styles.button}>Comenzar</p>
+              </Rounded>
+            </div>
+          </div>
+          <div className={styles.imageContainer}>
+            <img src="/images/hero.jpg" alt="Hero Image" className={styles.image} />
           </div>
         </div>
-        <div className={styles.imageContainer}>
-          <img src="/images/hero.jpg" alt="Hero Image" className={styles.image} />
-        </div>
       </div>
-      </div>
-
     </section>
   );
 };
