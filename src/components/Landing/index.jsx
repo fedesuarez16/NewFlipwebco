1be4aab2676eccdae8651097c19dec413bf14a46
fedesuarez16 <fedesuarez16@gmail.com>
2024-08-12 02:13:36 -0300

    idk

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -45,7 +45,11 @@ export default function Home() {
 
   return (
     <motion.main variants={slideUp} initial="initial" animate="enter" className={styles.landing}>
-    
+      <Image 
+        src="/images/bannerHeroGrid.png"
+        fill={true}
+        alt="background"
+      />
       <div className={styles.sliderContainer}>
         <div ref={slider} className={styles.slider}>
           <p ref={firstText}>FlipWebco - </p>
@@ -57,7 +61,7 @@ export default function Home() {
           <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
         </svg>
         <h1>Maximiza tu impacto & eficiencia - <br /> con nuestras soluciones Software</h1>
-      </div>
+        <p>creamos experiencias que optimizan la <b>eficiencia</b> , <b>productividad</b> y <b>satisfaccion</b> de los usuarios </p>      </div>
     </motion.main>
   )
 }
