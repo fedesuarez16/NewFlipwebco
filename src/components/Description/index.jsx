b860c25ecf0c195992dd118aafe49007c5a28887
fedesuarez16 <fedesuarez16@gmail.com>
2024-09-06 21:28:49 -0300

    idk

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -5,38 +5,44 @@ import { slideUp, opacity } from './animation';
 import Rounded from '../../common/RoundedButton';
 import Image from 'next/image';
 
-
-
 export default function index() {
-
-    const phrase = "Impulsando crecimiento de negocios & marcas a traves del desarrollo de productos digitales exepcionales. ";
+    const phrase = "Impulsando crecimiento de negocios & marcas a traves del desarrollo de productos digitales excepcionales.";
     const description = useRef(null);
-    const isInView = useInView(description)
+    const isInView = useInView(description);
+
     return (
-        <div id='about'  ref={description} className={styles.description}>
+        <div id='about' ref={description} className={styles.description}>
             <div className={styles.body}>
-                <p>
-                {
-                    phrase.split(" ").map( (word, index) => {
-                        return <span key={index} className={styles.mask}><motion.span variants={slideUp} custom={index} animate={isInView ? "open" : "closed"} key={index}>{word}</motion.span></span>
-                    })
-                }
-                </p>
-                <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>Somos la convergencia entre la eficiencia, la tecnología e innovación.</motion.p>
-                <a href='#contact'data-scroll data-scroll-speed={0.1}>
+                <div className={styles.text}>
+                    <p>
+                        {phrase.split(" ").map((word, index) => (
+                            <span key={index} className={styles.mask}>
+                                <motion.span
+                                    variants={slideUp}
+                                    custom={index}
+                                    animate={isInView ? "open" : "closed"}
+                                    key={index}
+                                >
+                                    {word}
+                                </motion.span>
+                            </span>
+                        ))}
+                    </p>
+                </div>
+                <a href='#contact' data-scroll data-scroll-speed={0.1}>
                     <Rounded className={styles.button}>
-                        <p> 
-                        <Image 
-                            src="/images/logosolo.png"
-                            alt="background"
-                            height={70} // Specify the numeric height value in pixels
-                            width={70} // Specify the numeric width value in pixels
-                            style={{ display: 'block' }} // Add inline styles as needed
-                        />  
+                        <p>
+                            <Image
+                                src="/images/logosolo.png"
+                                alt="background"
+                                height={70}
+                                width={70}
+                                style={{ display: 'block' }}
+                            />
                         </p>
                     </Rounded>
                 </a>
             </div>
         </div>
-    )
+    );
 }
