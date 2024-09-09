c754783312d39dc30ed62cc16ba48f3b7ff4bb9a
fedesuarez16 <fedesuarez16@gmail.com>
2024-09-09 14:23:27 -0300

    idk

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,75 +1,75 @@
+import { useState, useEffect } from 'react';
 import styles from './style.module.scss';
 import { Inter } from 'next/font/google';
 import { TypeAnimation } from 'react-type-animation'; 
 import { useInView } from 'react-intersection-observer';
-import Rounded from '../../common/RoundedButton';
 import Image from 'next/image';
-
 
 const inter = Inter({ subsets: ['latin'] });
 
-export default function Hero() {
-  const handleWhatsappClick = () => {
-    window.open('https://api.whatsapp.com/send?phone=5491133370937', '_blank');
-  };
+const texts = [
+  { title: 'Innovación Tecnológica', description: 'Desarrollamos soluciones que aprovechan las últimas tecnologías para transformar tu negocio.' },
+  { title: 'Soluciones a Medida', description: 'Creamos software personalizado para ajustarse a las necesidades específicas de tu empresa.' },
+  { title: 'Resultados Garantizados', description: 'Nos aseguramos de que nuestras soluciones ofrezcan resultados efectivos y medibles.' }
+];
 
-  const [inViewRef, inView] = useInView({
-    triggerOnce: true,
-    threshold: 0.1,
-  });
+export default function Hero() {
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const [isFading, setIsFading] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setIsFading(true); // Iniciar desvanecimiento
+      setTimeout(() => {
+        setCurrentTextIndex((prevIndex) => (prevIndex + 1) % texts.length);
+        setIsFading(false); // Resetear la animación
+      }, 500); // Duración de la animación de desvanecimiento
+    }, 3500); // Cambiar cada 3.5 segundos
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <section className={inter.className}>
-      <div className={styles.hero} ref={inViewRef}>
-        <div className={styles.text}>
-          <p> 3 enfoques para tu transformacion  
-            {inView && (
-              <TypeAnimation
-                sequence={[
-                  ' digital',
-                  1000,
-                ]}
-                speed={60}
-                wrapper='b'
-                repeat={Infinity}
-              /> 
-            )}
-          </p>
-        </div>
+      <div className={styles.hero}>
+      <div className={styles.text}>
+        <p> Reinventa la experiencia
+          <TypeAnimation
+            sequence={[
+              ' aplicaciones potenciadas por la IA',
+              1000,
+            ]}
+            speed={60}
+            wrapper='b'
+            repeat={Infinity}
+          />
+        </p>
+      </div>
 
-        {/* Nueva sección con tres recuadros conectados */}
+
+        {/* Nueva sección con un recuadro que cambia */}
         <div className={styles.boxContainer}>
           <div className={styles.line}></div> {/* Línea que conecta los recuadros */}
           
-          <div className={styles.box}>
-            <h3>Innovación Tecnológica</h3>
-            <p>Desarrollamos soluciones que aprovechan las últimas tecnologías para transformar tu negocio.</p>
+          <div className={`${styles.box} `}>
+            <h3>{texts[currentTextIndex].title}</h3>
+            <p>{texts[currentTextIndex].description}</p>
           </div>
 
-          <div className={styles.box}>
-            <h3>Soluciones a Medida</h3>
-            <p>Creamos aplicaciones personalizadas que responden a las necesidades específicas de tu empresa.</p>
-          </div>
-
-          <div className={styles.box}>
-            <h3>Resultados Garantizados</h3>
-            <p>Mejora la eficiencia y rentabilidad con nuestras herramientas impulsadas por IA.</p>
-          </div>
-          <div className={styles.boton} >
-          <a href='#contact' >
-                        <p>
-                            <Image
-                                src="/images/logosoloblanco.png"
-                                alt="background"
-                                height={70}
-                                width={70}
-                                style={{ display: 'block' }}
-                            />
-                        </p>
-                </a>
+          <div className={styles.boton}>
+            <a href='#contact'>
+              <p>
+                <Image
+                  src="/images/logosoloblanco.png"
+                  alt="background"
+                  height={70}
+                  width={70}
+                  style={{ display: 'block' }}
+                />
+              </p>
+            </a>
           </div>
         </div>
-
       </div>
     </section>
   );
