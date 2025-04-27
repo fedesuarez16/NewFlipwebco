96909cb64fb24ad95a365fcc0c1989fbb740d66b
fedesuarez16@gmail.com <federicosuarez@federicos-MacBook-Air.local>
2025-04-27 14:59:35 -0300

    idk

--- src/components/Clients/index.jsx
+++ src/components/Clients/index.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
 import styles from './style.module.scss';
 
 // Define tus propios logos (reemplaza las rutas con las de tus propios logos)
@@ -17,43 +14,27 @@ const clientLogos = [
   '/images/sinai.png',
   '/images/delsur.png',
   '/images/bj.png',
-
+  '/images/nunez.png',
+  '/images/opting.png',
 
   // Agrega más logos según sea necesario
 ];
   
-const ClientCarousel = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 1900,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    autoplay: true, // Hacer que el carrusel se desplace automáticamente
-    autoplaySpeed: 400, // Velocidad de desplazamiento automático en milisegundos
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 5, // Mostrar cinco logos a la vez en dispositivos móviles
-        },
-      },
-    ],
-  };
-
+const ClientsGrid = () => {
   return (
-    <div className={styles.clientCarousel}>
+    <div className={styles.clientsSection}>
       <h2 className={styles.title}>Marcas que nos eligen</h2>
       <p className={styles.description}>Empresas que eligieron reinventarse con nosotros.</p>
-      <Slider {...settings}>
+      
+      <div className={styles.logosGrid}>
         {clientLogos.map((logo, index) => (
           <div key={index} className={styles.logoContainer}>
             <img src={logo} title={`Client Logo ${index + 1}`} alt={`Client Logo ${index + 1}`} />
           </div>
         ))}
-      </Slider>
+      </div>
     </div>
   );
 };
 
-export default ClientCarousel;
+export default ClientsGrid;
--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -49,11 +49,10 @@ export default function Home() {
        <div ref={slider} className={styles.slider}>
         <p ref={firstText}>Flipwebco software - </p>
         <p ref={secondText}>Flipwebco software - </p>
-
       </div> 
     </div>
     <div data-scroll data-scroll-speed={0.1} className={styles.descriptionContainer}>
-      <div className={styles.textWithImage}>
+      <div className={styles.content}>
         <div className={styles.text}>
           <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
@@ -63,7 +62,7 @@ export default function Home() {
           <p>Creamos experiencias que optimizan la <b>eficiencia</b> , <b>productividad</b> y <b>satisfacción</b> de los usuarios</p>
         </div>
         <div className={styles.imageContainer}>
-         
+          <div className={styles.blurEffect}></div>
           <Image
             src="/images/hero.png"
             width={500}
